25f613d87523fc2f5f7619e0a2db90d677205ce4
Joe Simms <joe@elevateatlantic.ca>
2016-07-10 23:03:51 -0300

    removed readme

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,13 @@
 class Photo < ActiveRecord::Base
   validates :external_id, presence: true
   validates :search_term, presence: true
+
+  def self.print_click_count search_term
+    output = "SEARCH TERM: #{search_term}"
+    Photo.where(search_term: search_term).each do |p|
+      output = "#{output}\nEXTERNAL_ID: #{p.external_id} CLICK_COUNT: #{p.click_count}"
+    end
+    output = "#{output}\nEND REPORT"
+    Rails.logger.info output
+  end
 end
